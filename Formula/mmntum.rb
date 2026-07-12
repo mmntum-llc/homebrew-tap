5fd7b3fe65158506dba490e40c2c92e323ac82bd
MMNTUM Release <releases@mmntum.ai>
2026-07-11 20:50:21 -0600

    mmntum v0.7.0

--- Formula/mmntum.rb
+++ Formula/mmntum.rb
@@ -7,22 +7,22 @@ class Mmntum < Formula
   on_macos do
     on_intel do
       url "https://shiftstack.ai/dl/mmntum/v0.7.0/mmntum_darwin_amd64"
-      sha256 "546166911182443d798415220bd1b2d747f6869a6b56392f8c4a5f27c95b10ff"
+      sha256 "af041e5b17c0ec53447dd510473f2a66c895427528f23339f22b3c3911122f2c"
     end
     on_arm do
       url "https://shiftstack.ai/dl/mmntum/v0.7.0/mmntum_darwin_arm64"
-      sha256 "c27f851a0e7793a118348364b1715583445c6d2ea31a8c9203de8da28c547a13"
+      sha256 "aefe959719aedc0e78074440cba2ac49c4df4321309f185446f175416cc87f70"
     end
   end
 
   on_linux do
     on_intel do
       url "https://shiftstack.ai/dl/mmntum/v0.7.0/mmntum_linux_amd64"
-      sha256 "61e0a6b3a477c9935477a166e8e2346cf8df2cd3add35edb627d56604e8058f1"
+      sha256 "59534076de569f0281d8cdc674c28e803c8df1613c40f3403bc72a8e2fe3e275"
     end
     on_arm do
       url "https://shiftstack.ai/dl/mmntum/v0.7.0/mmntum_linux_arm64"
-      sha256 "3d06f7d8a2f656432034706acc44aa7f37247526bd248905fc5c91d5b30df5c4"
+      sha256 "7b0d4806b9184bd6ef27d1d5effdae2f579467ac021528a7e5171975f241d798"
     end
   end
 
